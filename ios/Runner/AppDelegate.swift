a439b16febdb4b6f6240603546044c0d890dd5e9
Filipe Barroso <filipe.barroso@gmail.com>
2019-08-30 00:09:23 +0100

    Add first iteration of a Native splash screen

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+    UIApplication.shared.isStatusBarHidden = false;
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
