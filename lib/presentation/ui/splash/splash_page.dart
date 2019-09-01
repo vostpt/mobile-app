7fbca371a71e621948317f220a66664f3bbeab5e
Filipe Barroso <filipe.barroso@gmail.com>
2019-09-01 22:46:01 +0100

    Adds Splash navigation to Home after a timer.
    After a couple a seconds the Splash screen will navigate to the Home

--- lib/presentation/ui/splash/splash_page.dart
+++ lib/presentation/ui/splash/splash_page.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:vost/presentation/navigation/navigation.dart';
 
 class SplashPage extends StatefulWidget {
   SplashPage({Key key, this.title}) : super(key: key);
+
   final String title;
+
   @override
   _MySplashPageState createState() => _MySplashPageState();
 }
@@ -21,5 +26,25 @@ class _MySplashPageState extends State<SplashPage> {
         ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    timedNavigationTo(routeHome);
+  }
+
+  /*
+   * After a time it will navigate to the home screen.
+   * Since this is just a Splash screen, it shouldn't be shown for a long time.
+   */
+  void timedNavigationTo(String newPage) {
+    /*
+    The timer set for 2400 is just enough time to see the whole animation
+      this should be modified to better fit the navigation.
+     */
+    Timer(Duration(milliseconds: 2400), () {
+      Navigator.pushReplacementNamed(context, routeHome);
+    });
   }
 }
