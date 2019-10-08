10cd9175cd02abdf1ff2e0a2ccb5f2165bb7692f
sg23x <soumyagupta2307@gmail.com>
2019-10-08 19:46:50 +0530

    fixed packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vost/di/dependency_provider.dart';
 import 'package:vost/presentation/app.dart';
+import 'dart:async';
 
 void main() async {
   var dependencyProvider = DependencyProvider(child : VostApp());
