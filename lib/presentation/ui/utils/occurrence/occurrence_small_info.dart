39f96eceb23470656f9ccb60d824a0dec0a79149
CodeGlitch <CodeGlitch@pm.me>
2019-11-13 21:19:44 -0100

    Switched Widget to use OccurrenceWidgetBackground

--- lib/presentation/ui/utils/occurrence/occurrence_small_info.dart
+++ lib/presentation/ui/utils/occurrence/occurrence_small_info.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:vost/constants.dart' as Constants;
-//import 'package:vost/presentation/assets/dimensions.dart';
 import 'package:vost/presentation/assets/text_styles.dart';
 import 'package:vost/presentation/utils/misc.dart';
 
-//import 'occurrence_widget_background.dart';
+import 'occurrence_widget_background.dart';
 
 class OccurrenceSmallInfoWidget extends StatelessWidget {
   final String _updatedAt;
@@ -16,15 +15,14 @@ class OccurrenceSmallInfoWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.bottomCenter,
-      child: Container(
-        margin: EdgeInsets.only(bottom: 70, left: 10, right: 10),
+      child: OccurrenceWidgetBackground(
         child: ListView(
           shrinkWrap: true,
           children: <Widget>[
             Container(
-              padding: EdgeInsets.only(bottom: 35),
+              margin: EdgeInsets.only(bottom: 30),
               color: Colors.white,
-              child: ListTile(
+              child: ListTile(contentPadding: EdgeInsets.only(bottom: 15),
                 title: Text(
                   getFormattedDate(
                       DateTime.parse(_updatedAt), Constants.FORMAT_DATE_DAY),
@@ -40,6 +38,5 @@ class OccurrenceSmallInfoWidget extends StatelessWidget {
         ),
       ),
     );
-    ;
   }
 }
